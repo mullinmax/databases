4d4a222a878a6d627686d34fc5cea7c20b8c2392
maxwell <mullinmax@gmail.com>
2018-04-03 01:57:48 -0400

    editing script

--- q1.sql
+++ q1.sql
@@ -6,3 +6,20 @@ WHERE	C.LOYALTYCARDID = T.CUSTID and
 		T.TACOID = TACO.TACOID and 
 		TACO.TACOID = M.TACOID and
 		M.INGREDIENT = 'Chicken';
+
+/*
+SQL> @q1.sql
+
+NAME                  SALEPRICE
+-------------------- ----------
+Carlton                       5
+Joe                        7.15
+Sam                         4.3
+Amy                           5
+Sarah                       5.5
+Emma                          6
+Brittney                    7.2
+
+7 rows selected.
+
+*/
--- q3.sql
+++ q3.sql
@@ -2,3 +2,25 @@
 
 SELECT NAME, SERVINGSONHAND
 FROM INGREDIENT;
+
+/*
+SQL> @q3.sql
+
+NAME                 SERVINGSONHAND
+-------------------- --------------
+Salsa                          2000
+Chicken                        2000
+Ground Beef                    2000
+Steak Beef                     2000
+Cheese                         2000
+Sour Cream                     2000
+Black Beans                    2000
+Pinto Beans                    2000
+Lettus                         2000
+Corn Tortilla                  2000
+Corn Shell                     2000
+Flour Tortilla                 2000
+
+12 rows selected.
+
+*/
