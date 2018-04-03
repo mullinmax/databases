04bac6c9b0351a73f29ad41b50e2824d383d719d
maxwell <mullinmax@gmail.com>
2018-04-03 01:53:33 -0400

    editing script

--- q1.sql
+++ q1.sql
@@ -1,4 +1,6 @@
-SELECT C.NAME
+-- lists the names of every customer the bought a taco with chicken and what they paid for it
+
+SELECT C.NAME T.SALEPRICE
 FROM CUSTOMER C, TRANSACTION T, TACO, MADEWITH M
 WHERE	C.LOYALTYCARDID = T.CUSTID and 
 		T.TACOID = TACO.TACOID and 
