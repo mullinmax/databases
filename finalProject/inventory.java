7794a258a07b83dc1626c737eeee8b45ff662cb0
Maxwell <mullinmax@gmail.com>
2018-04-24 13:54:29 -0400

    completely re-written inventory

--- finalProject/inventory.java
+++ finalProject/inventory.java
@@ -1,15 +1,14 @@
-import java.sql.*; 
-import java.io.*; 
+import java.sql.*;
+import java.io.*;
 
-class inventory { 
+class inventory {
   void displayInventory(Connection conn) throws SQLException, IOException {
-    String query = "select * from INGEEDIENT";
+    String query = "select * from INGREEDIENT";
     Statement stmt = conn.createStatement();
     ResultSet rset = stmt.executeQuery(query);
     System.out.println("");
-    while (rset.next ()) { 
-      System.out.println(rset.getString(1) + "   " + rset.getString(2) + "   " + rset.getString(3));
-    } 
-    System.out.println("");
+    while (rset.next()){
+      System.out.println(rset.getString(1));
+    }
   }
 }
