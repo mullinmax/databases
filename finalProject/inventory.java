f9ca1588bbc03ad29ac94c8c82faffe42129e6f9
Maxwell <mullinmax@gmail.com>
2018-04-24 12:22:12 -0400

    testing for culprit line

--- finalProject/inventory.java
+++ finalProject/inventory.java
@@ -11,7 +11,7 @@ import java.io.*;
 class inventory { 
 
   void displayInventory(Connection conn) throws SQLException, IOException {
-    String query = ";";
+    String query = "select * from INGREDIENT;";
     Statement stmt = conn.createStatement(); 
     ResultSet rset = stmt.executeQuery(query);
     System.out.println("");
