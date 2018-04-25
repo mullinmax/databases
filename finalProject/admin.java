83fca62c5bb0e8be8797131c4494b88def083c4b
Maxwell <mullinmax@gmail.com>
2018-04-25 10:50:10 -0400

    summing price

--- finalProject/admin.java
+++ finalProject/admin.java
@@ -19,19 +19,12 @@ class admin {
 	}
 
 	void sales(Connection conn, String date) throws SQLException, IOException {
-		String query = "select * from TRANSACTION where DAY = '" + date + "'";
+		String query = "select SUM(PRICE) from TRANSACTION where DAY = '" + date + "'";
 		Statement stmt = conn.createStatement();
 		ResultSet rset = stmt.executeQuery(query);
 		System.out.println("");
 		while (rset.next()){
 			System.out.printf("%-20s", rset.getString(1));
-			System.out.printf("%-6s", rset.getString(2));
-			System.out.printf("%-6s", rset.getString(3));
-			System.out.printf("%-6s", rset.getString(4));
-			System.out.printf("%-6s", rset.getString(5));
-			System.out.printf("%-6s", rset.getString(6));
-			System.out.printf("%-6s", rset.getString(7));
-			System.out.printf("%-6s", rset.getString(8));
 			System.out.println("");
 		}
 		System.out.println("");
