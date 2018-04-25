b96b68f5ccf4c312f00480011ade556135f78611
Maxwell <mullinmax@gmail.com>
2018-04-25 12:38:54 -0400

    getting cost and sale price for sales report

--- finalProject/admin.java
+++ finalProject/admin.java
@@ -17,7 +17,7 @@ class admin {
 	}
 
 	void salesReport(Connection conn) throws SQLException, IOException {
-		String query = "SELECT SUM(SALEPRICE), NAME FROM (SELECT * FROM TRANSACTION T, EMPLOYEE E WHERE T.EMPID = E.SSN) GROUP BY EMPID, NAME ORDER BY EMPID";
+		String query = "SELECT SUM(P.PRICE), SUM(T.SALEPRICE), E.NAME FROM EMPLOYEE E, TRANSACTION T, (SELECT SUM(I.BULKPRICE) AS PRICE, M.TACOID FROM INGREDIENT I, MADEWITH M WHERE I.NAME = M.INGREDIENT GROUP BY M.TACOID) P WHERE P.TACOID = T.TACOID AND E.SSN = T.EMPID GROUP BY E.NAME, E.SSN";
 
 		Statement stmt = conn.createStatement();
 		ResultSet rset = stmt.executeQuery(query);
@@ -25,6 +25,7 @@ class admin {
 		while (rset.next()){
 			System.out.printf("%-20s", rset.getString(1));
 			System.out.printf("%-20s", rset.getString(2));
+			System.out.printf("%-20s", rset.getString(3));
 			System.out.println("");
 		}
 		System.out.println("");
