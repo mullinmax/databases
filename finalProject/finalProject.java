efbfdf85d1490d5b790befd09f8040b2945320da
Maxwell <mullinmax@gmail.com>
2018-04-24 22:40:26 -0400

    fixing weird character problem

--- finalProject/finalProject.java
+++ finalProject/finalProject.java
@@ -3,67 +3,67 @@ import java.sql.*;
 
 class tacoStandDatabase { 
 
-  public static void main (String args []) 
-      throws SQLException, IOException { 
+	public static void main (String args []) 
+			throws SQLException, IOException { 
 
-    inventory warehouse = new inventory();
-    boolean done;
-    char ch,ch1;
-    byte num = 0;
+		inventory warehouse = new inventory();
+		boolean done;
+		char ch,ch1;
+		byte num = 0;
 
-    try {
-      Class.forName ("oracle.jdbc.driver.OracleDriver");
-    } catch (ClassNotFoundException e) {
-        System.out.println ("Could not load the driver");
-        return;
-      }
-    String user, pass;
-    user = readEntry("userid  : ");
-    pass = readEntry("password: ");
+		try {
+			Class.forName ("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+				System.out.println ("Could not load the driver");
+				return;
+			}
+		String user, pass;
+		user = readEntry("userid	: ");
+		pass = readEntry("password: ");
 
-    //  The following line was modified by Prof. Marling to work on prime
+		//	The following line was modified by Prof. Marling to work on prime
 
-    Connection conn = DriverManager.getConnection
-       ("jdbc:oracle:thin:@deuce.cs.ohiou.edu:1521:class", user, pass);
+		Connection conn = DriverManager.getConnection
+			("jdbc:oracle:thin:@deuce.cs.ohiou.edu:1521:class", user, pass);
 
-    done = false;
-    do {
-      System.out.println("Welcome to the Taco Stand Database. Please select a task:");
-      System.out.println(" - Display warehouse inventory (1):");
-      System.out.flush();
-      ch = (char) System.in.read();
-      ch1 = (char) System.in.read();
-      switch (ch) {
-        case '1' : 
-          warehouse.displayInventory(conn);
-          break;
-        case '2' : 
-          String ingredient = readEntry("What ingredient is being received?");
-          String numServings = readEntry("how many servings of " + ingredient + "?");
-          warehouse.receiveShipment(conn, ingredient, numServings);
-        case 'q' : done = true;
-                   break;
-        default  : System.out.println("Type in option again");
-      }
-    } while (!done);
+		done = false;
+		do {
+			System.out.println("Welcome to the Taco Stand Database. Please select a task:");
+			System.out.println(" - Display warehouse inventory (1):");
+			System.out.flush();
+			ch = (char) System.in.read();
+			ch1 = (char) System.in.read();
+			switch (ch) {
+				case '1' : 
+					warehouse.displayInventory(conn);
+					break;
+				case '2' : 
+					String ingredient = readEntry("What ingredient is being received?");
+					String numServings = readEntry("how many servings of " + ingredient + "?");
+					warehouse.receiveShipment(conn, ingredient, numServings);
+				case 'q' : done = true;
+									break;
+				default	: System.out.println("Type in option again");
+			}
+		} while (!done);
 
-    conn.close();
-  }
+		conn.close();
+	}
 
-  //readEntry function -- to read input string
-  static String readEntry(String prompt) {
-     try {
-       StringBuffer buffer = new StringBuffer();
-       System.out.print(prompt);
-       System.out.flush();
-       int c = System.in.read();
-       while(c != '\n' && c != -1) {
-         buffer.append((char)c);
-         c = System.in.read();
-       }
-       return buffer.toString().trim();
-     } catch (IOException e) {
-       return "";
-     }
-   }
+	//readEntry function -- to read input string
+	static String readEntry(String prompt) {
+		try {
+			StringBuffer buffer = new StringBuffer();
+			System.out.print(prompt);
+			ystem.out.flush();
+			int c = System.in.read();
+			while(c != '\n' && c != -1) {
+				buffer.append((char)c);
+				c = System.in.read();
+			}
+			return buffer.toString().trim();
+		} catch (IOException e) {
+			return "";
+		}
+	}
 }
--- finalProject/inventory.java
+++ finalProject/inventory.java
@@ -2,34 +2,34 @@ import java.sql.*;
 import java.io.*;
 
 class inventory {
-  void displayInventory(Connection conn) throws SQLException, IOException {
-    String query = "select * from INGREDIENT";
-    Statement stmt = conn.createStatement();
-    ResultSet rset = stmt.executeQuery(query);
-    System.out.println("");
-    while (rset.next()){
-      System.out.printf("%-20s", rset.getString(1));
-      System.out.printf("%-6s", rset.getString(2));
-      System.out.printf("%-6s", rset.getString(3));
-      System.out.println("");
-    }
-    System.out.println("");
-  }
+	void displayInventory(Connection conn) throws SQLException, IOException {
+		String query = "select * from INGREDIENT";
+		Statement stmt = conn.createStatement();
+		ResultSet rset = stmt.executeQuery(query);
+		System.out.println("");
+		while (rset.next()){
+			System.out.printf("%-20s", rset.getString(1));
+			System.out.printf("%-6s", rset.getString(2));
+			System.out.printf("%-6s", rset.getString(3));
+			System.out.println("");
+		}
+		System.out.println("");
+	}
 
-  void receiveShipment(Connection conn, String ingredient, String numServings) throws SQLException, IOException {
-    String query = "INSERT INTO INGREDIENT VALUES ('" + ingredient + "', 0.05, " + numServings + ");";
-    Statement stmt = conn.createStatement(); 
-    try {
-      stmt.executeUpdate(query);
-    } catch (SQLException e) {
-        System.out.println("Ingredient was not added! Failure!");
-        while (e != null) {
-          System.out.println("Message     : " + e.getMessage());
-          e = e.getNextException();
-        }
-        return;
-    }
-    System.out.println("Shipment received");
-    stmt.close();
-  }
+	void receiveShipment(Connection conn, String ingredient, String numServings) throws SQLException, IOException {
+		String query = "INSERT INTO INGREDIENT VALUES('" + ingredient + "', 0.05, " + numServings + ");";
+		Statement stmt = conn.createStatement();
+		try{
+			stmt.executeUpdate(query);
+		}catch(SQLException e) {
+			System.out.print("Error:");
+			while(e != null) {
+				System.out.println(e.getMessage());
+				e = e.getNextException();
+			}
+			return;
+		}
+		System.out.println("Shipment received");
+		stmt.close();
+	}
 }
