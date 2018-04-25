2e55f75ee4ea9464eb2db6db2cda34da87f1ac11
Maxwell <mullinmax@gmail.com>
2018-04-25 01:55:10 -0400

    adding sales function

--- finalProject/admin.java
+++ finalProject/admin.java
@@ -19,18 +19,16 @@ class admin {
 	}
 
 	void sales(Connection conn, String date) throws SQLException, IOException {
-		/*String query = "DELETE INGREDIENT WHERE NAME = '" + ingredient + "'";
-		try{
-			stmt.executeUpdate(query);
-		}catch(SQLException e) {
-			System.out.print("Statement Error:");
-			while(e != null) {
-				System.out.println(e.getMessage());
-				e = e.getNextException();
-			}
-			return;
+		String query = "select * from TRANSACTION";
+		Statement stmt = conn.createStatement();
+		ResultSet rset = stmt.executeQuery(query);
+		System.out.println("");
+		while (rset.next()){
+			System.out.printf("%-20s", rset.getString(1));
+			System.out.printf("%-6s", rset.getString(2));
+			System.out.printf("%-6s", rset.getString(3));
+			System.out.println("");
 		}
-		System.out.println("Ingredient removed");
-		stmt.close();*/
+		System.out.println("");
 	}
 }
